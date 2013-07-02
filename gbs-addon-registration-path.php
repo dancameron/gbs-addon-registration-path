c7a0fb0b53f223c259adeb03310e526a131cf8a2
Dan Cameron <dan@sproutventure.com>
2013-07-02 09:41:11 -0700

    plugin.

--- gbs-addon-registration-path.php
+++ gbs-addon-registration-path.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Dynamic Donations
-Version: 1.3
+Plugin Name: Advanced Merchant Registration
+Version: 1
 Plugin URI: http://groupbuyingsite.com/marketplace
-Description: Adds a dynamic charity selection to the checkout for customers with the ability for charity templates. Allow the customer to select how much their donation should be by adding an item to their cart; removes the selection area added with the basic dynamic charities above.
+Description: Merchant registration includes account registration if not logged in.
 Plugin URI: http://groupbuyingsite.com/marketplace/
 Author: Sprout Venture
 Author URI: http://sproutventure.com/
@@ -13,17 +13,17 @@ Contributors: Dan Cameron
 Text Domain: group-buying
 */
 
-define ('GB_DYN_CHARITY_URL', plugins_url( '', __FILE__) );
-define( 'GB_DYN_CHARITY_PATH', WP_PLUGIN_DIR . '/' . basename( dirname( __FILE__ ) ) );
+define ('GB_SSB_REG_URL', plugins_url( '', __FILE__) );
+define( 'GB_SSB_REG_PATH', WP_PLUGIN_DIR . '/' . basename( dirname( __FILE__ ) ) );
 
 // Load after all other plugins since we need to be compatible with groupbuyingsite
-add_action( 'plugins_loaded', 'gb_load_bundles' );
-function gb_load_bundles() {
+add_action( 'plugins_loaded', 'gb_load_custom_registration' );
+function gb_load_custom_registration() {
 	$gbs_min_version = '4.3';
 	if ( class_exists( 'Group_Buying_Controller' ) && version_compare( Group_Buying::GB_VERSION, $gbs_min_version, '>=' ) ) {
-		require_once 'classes/GBS_Dynamic_Charities_Addon.php';
+		require_once 'classes/GBS_Registration_Addon.php';
 
 		// Hook this plugin into the GBS add-ons controller
-		add_filter( 'gb_addons', array( 'GBS_Dynamic_Charities_Addon', 'gb_addon' ), 10, 1 );
+		add_filter( 'gb_addons', array( 'GBS_Registration_Addon', 'gb_addon' ), 10, 1 );
 	}
 }
